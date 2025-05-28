9601609c4d3c111dbdf7a21c58e407d2de3b2de2
Divaon <56230474+Divaon@users.noreply.github.com>
2025-05-28 19:40:54 +0300

    Update Task4.cpp

--- Module6/Task4/Task4.cpp
+++ Module6/Task4/Task4.cpp
@@ -8,29 +8,27 @@
 
 int main()
 {
-
-
     std::cout << "Task4" << std::endl;
 
-    std::vector<int> Vec{};
+    int* Array = new int[10];
 
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; ++i) {
         int number = 0;
         std::cout << "Enter a number: ";
         std::cin >> number;
         std::cout << std::endl;
-        Vec.push_back(number);
+        Array[i] = number;
     }
 
 
-    for (int i = 0; i < Vec.size(); i++) {
-        std::cout << Vec[i] << " ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << Array[i] << " ";
     }
 
     std::cout << std::endl;
-    std::cout << "Min: " << *std::min_element(begin(Vec), end(Vec)) << std::endl;
-    std::cout << "Max: " << *std::max_element(begin(Vec), end(Vec)) << std::endl;
-    std::cout << std::endl;
+    std::cout << "Min: " << *std::min_element(Array, Array + 10) << std::endl;
+    std::cout << "Max: " << *std::max_element(Array, Array + 10) << std::endl;
 
+    std::cout << std::endl;
 }
